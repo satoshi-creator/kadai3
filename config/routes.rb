04a777c9ff7884a70be7f9ab44883d2ebede6c76
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-11 06:35:18 +0000

    [Finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
 devise_for :users
 root to: "homes#top"
-get 'homes/about' => 'homes#about', as: 'about'
 
+resources :books, only: [:new, :index, :create, :show, :edit, :destroy]
+
+get 'homes/about' => 'homes#about', as: 'about'
+patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
