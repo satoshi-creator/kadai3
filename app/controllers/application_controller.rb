461cf1871c903268f4f1ad33ba0801d89431ac3e
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-13 13:58:46 +0000

    validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! You have signed up successfully."
+    user_path(@user.id)
+  end
+
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
     user_path(@user.id)
