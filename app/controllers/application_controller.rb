803df4a789f7c028f40d009618895c4086dbd629
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-13 07:56:49 +0000

    fix_introduction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    books_path
+    flash[:notice] = "Signed in successfully."
+    user_path(@user.id)
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    flash[:notice] = "Signed out successfully."
+    "/"
   end
 
   protected
