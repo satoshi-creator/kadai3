c586f6b86c7fe2c0c466397aacc4bd5811155b4c
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-13 05:51:52 +0000

    fix_books_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   def index
     @users = User.all
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
-  
+
   has_one_attached :profile_image
-  
+
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images 2/sample-author1.jpg')
