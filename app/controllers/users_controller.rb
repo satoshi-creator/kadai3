d404c7f19fffb5ebcd121a33d5dd03f53318c22a
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-14 09:02:09 +0000

    layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,8 @@ get 'homes/about' => 'homes#about', as: 'about'
 
 resources :users, only: [:show, :edit, :update, :index]
 
+resources :books, only: [:new, :index, :create, :show, :edit, :update, :destroy]
 
 
-resources :books, only: [:new, :index, :create, :show, :edit, :destroy]
-
-
-patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
